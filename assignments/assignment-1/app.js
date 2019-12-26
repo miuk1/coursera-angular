65cb3d882c280a6cc6071b1702bd10aebcd94a19
mukesh <samu.mg60@gmail.com>
2019-12-26 09:26:35 +0530

    completed assignment 1

---
+++ assignments/assignment-1/app.js
@@ -0,0 +1,30 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('lunchChecker', [
+
+        ])
+        .controller('lunchCheckerController', function ($scope) {
+            var vm = $scope;
+            vm.lunch = '';
+            vm.lunches = [];
+            vm.msg;
+
+            vm.checkLunch = function () {
+                if (vm.lunch == '') {
+                    vm.msg = "Please enter data first";
+                } else {
+                    vm.lunches = vm.lunch.split(',').filter(word => word.length > 1);
+                    if (vm.lunches.length <= 3 && vm.lunches.length > 0) {
+                        vm.msg = "Enjoy!";
+                    } else if (vm.lunches.length > 3) {
+                        vm.msg = "Too much!";
+                    } else {
+                        vm.msg = "Please enter data first";
+                    }
+                }
+            }
+        });
+
+})();
